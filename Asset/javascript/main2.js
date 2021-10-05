43eb20b5dd12249aa1466ba98c24fe865570fa32
touseef75 <imrantouseef776@gmail.com>
2021-10-04 21:34:20 -0700

    set

---
+++ Asset/javascript/main2.js
@@ -0,0 +1,45 @@
+
+let incre = document.getElementById("incre");
+
+let head2 = document.getElementById("head2");
+let dua2 = document.getElementById("duaimg");
+let meaning = document.getElementById("meaning");
+
+let audio = document.getElementById("audio");
+
+
+let imgarr = [
+    {
+        head2s:"سورة العصر",
+        meanings : "وَالۡعَصۡرِۙ.‏اِنَّ الۡاِنۡسَانَ لَفِىۡ خُسۡرٍۙ‏.اِلَّا الَّذِيۡنَ اٰمَنُوۡا وَ عَمِلُوۡا الصّٰلِحٰتِ<br>وَتَوَاصَوۡا بِالۡحَقِّ ۙ وَتَوَاصَوۡا بِالصَّبۡر",
+        img2 :  "../images/quraan.jpg",
+        audios : "../audio/surehAsr.mp3",
+    },
+   {
+        head2s:"سورة النصر",
+        meanings :"اِذَا جَآءَ نَصۡرُ اللّٰهِ وَالۡفَتۡحُۙ‏.وَرَاَيۡتَ النَّاسَ يَدۡخُلُوۡنَ فِىۡ دِيۡنِ اللّٰهِ اَفۡوَاجًاۙ <br> فَسَبِّحۡ بِحَمۡدِ رَبِّكَ وَاسۡتَغۡفِرۡهُ‌ؔؕ اِنَّهٗ كَانَ تَوَّابًا",
+        img2 :  "../images/quraan.jpg",
+        audios : "../audio/surehnsr.mp3",
+
+    },  
+]
+
+let num1 = 1;
+incre.addEventListener("click",function(){
+    if(num1 == 2){
+        num1 = 0;
+    head2.innerHTML = imgarr[num1].head2s;
+    meaning.innerHTML = imgarr[num1].meanings;
+    audio.src = imgarr[num1].audios;
+    
+    }else{
+    head2.innerHTML = imgarr[num1].head2s;
+    meaning.innerHTML = imgarr[num1].meanings;
+    audio.src = imgarr[num1].audios;
+
+
+    num1++;
+
+    }
+})
+
---
+++ Asset/javascript/main3.js
@@ -0,0 +1,41 @@
+
+let incre = document.getElementById("incre");
+
+let dua2 = document.getElementById("duaimg");
+
+let audio = document.getElementById("audio");
+
+
+
+let imgarr = [
+    {
+        img2 :  "../images/Kalmay-3.jpg",
+
+        audios : "../audio/3rd_kalima.mp3",
+
+    },
+   {
+
+        img2 :  "../images/Kalmay-4.jpg",
+        audios : "../audio/4th_kalima.mp3",
+
+    },
+]
+
+
+let num1 = 1;
+incre.addEventListener("click",function(){
+    if(num1 == 2){
+        num1 = 0;
+    dua2.src = imgarr[num1].img2;
+    audio.src = imgarr[num1].audios;
+
+    
+    }else{
+    dua2.src = imgarr[num1].img2;
+    audio.src = imgarr[num1].audios;
+
+    num1++;
+    }
+})
+
